d817ed7d0684c540363cd70e3ca95309dc7f44fd
maruf <dev.maruf112@gmail.com>
2023-01-27 03:01:39 +0600

    navbar items toggle active

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,12 @@ hamburger.onclick = function () {
     const navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active");
 }
+// toggle active class when clicking navbar items
+const navItems = document.querySelectorAll(".nav-item");
+navItems.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        navItems.forEach(f =>
+            f.classList.remove('active'));
+        e.target.classList.toggle("active");
+    })
+})

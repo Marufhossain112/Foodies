622be54bddce4dba5d7afcaeb74c90e75120d705
maruf <dev.maruf112@gmail.com>
2023-01-27 01:11:44 +0600

    navbar added

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+// toggle hamburger menu
+const hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function () {
+    const navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
